2d3e8f0f0360725fce263816ed91777b3051334d
Rodrigo Morais <morais.rm@gmail.com>
2015-05-28 19:56:40 -0300

    Adding vendor folder of dummy test in git track.

--- component/templates/templates.js
+++ component/templates/templates.js
@@ -2,36 +2,21 @@ angular.module('rmRightMenu').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('component/templates/angular-right-menu.html',
-    "<div class=\"menu\" style=\"background-color: {{backgroundColor}};\">\r" +
-    "\n" +
-    "    <div class=\"title\">{{title}}</div>\r" +
-    "\n" +
-    "    <ul>\r" +
-    "\n" +
-    "        <li\r" +
-    "\n" +
-    "            data-ng:repeat=\"menu in menus\"\r" +
-    "\n" +
-    "            data-ng:click=\"select(menu)\"\r" +
-    "\n" +
-    "        >\r" +
-    "\n" +
-    "            <a\r" +
-    "\n" +
-    "                href=\"{{ menu.link }}\"\r" +
-    "\n" +
-    "                data-ng:class=\"{'selected': menu.selected}\"\r" +
-    "\n" +
-    "            >\r" +
-    "\n" +
-    "                {{ menu.text }}\r" +
-    "\n" +
-    "            </a>\r" +
-    "\n" +
-    "        </li>\r" +
-    "\n" +
-    "    </ul>\r" +
-    "\n" +
+    "<div class=\"menu\" style=\"background-color: {{backgroundColor}};\">\n" +
+    "    <div class=\"title\">{{title}}</div>\n" +
+    "    <ul>\n" +
+    "        <li\n" +
+    "            data-ng:repeat=\"menu in menus\"\n" +
+    "            data-ng:click=\"select(menu)\"\n" +
+    "        >\n" +
+    "            <a\n" +
+    "                href=\"{{ menu.link }}\"\n" +
+    "                data-ng:class=\"{'selected': menu.selected}\"\n" +
+    "            >\n" +
+    "                {{ menu.text }}\n" +
+    "            </a>\n" +
+    "        </li>\n" +
+    "    </ul>\n" +
     "</div>"
   );
 
